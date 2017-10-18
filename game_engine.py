a260c86db77d884f83bb72c10d0d0133d7533f9d
Ezequiel Marques Ramos <ezequielmr94@gmail.com>
2017-10-18 01:04:02 -0200

    chuva de estrelas

--- game_engine.py
+++ game_engine.py
@@ -8,8 +8,9 @@ game engine class
 import pygame_sdl2
 pygame_sdl2.import_as_pygame()
 import pygame
-from game_object import game_object, button
+from game_object import game_object, button, estrela, Player
 import time
+import random
 
 class game_engine():
     #
@@ -17,7 +18,7 @@ class game_engine():
     #
     # Pygame variables
     SCREEN_SIZE = (500,500)
-    BACKGROUND_COLOR = (50,50,50)
+    BACKGROUND_COLOR = (1,1,1)
     CAPTION = "Sample Android Game"
     done = False
     fps = 30.0
@@ -59,8 +60,9 @@ class game_engine():
         #Add square to object list
 
         self.player = game_object(self.screen, self.SCREEN_SIZE, [486/2.0, 864/2.0], [0,0], self.OBJECT_COLOR, self.object_size)
+        #self.player = Player(486/2.0, 864/2.0, self.screen, self.SCREEN_SIZE)
 
-        self.objects.append(self.player)
+        #self.objects.append(self.player)
         
         #Reset floor height based on new screen size
         self.floor_height = 864 - 864/10.0
@@ -77,6 +79,10 @@ class game_engine():
         self.botaoDireita = button(self.screen, self.SCREEN_SIZE, (100,100,100), [(sizeX+2)+sizeX/2, 2+self.floor_height+sizeY/2], [sizeX, sizeY])
         self.objects.append(self.botaoDireita)
 
+        self.estrelas = []
+        for i in range(0,100):
+            self.estrelas.append(estrela(self.screen,self.SCREEN_SIZE,[random.randint(1,486),random.randint(1,864)]))
+
     """handle key presses"""
     def event_loop(self):
         #For each event that has happened(each keystroke):
@@ -86,9 +92,6 @@ class game_engine():
             #If the key is a left mouse click or android touch make the square jump
             if (event.type == pygame.MOUSEBUTTONDOWN) and event.button == 1:
                 x, y = event.pos
-
-                if y < self.floor_height * self.ratioY:
-                    self.objects[0].jump()
 
                 if y > self.floor_height * self.ratioY:
                     if x > self.SCREEN_SIZE[0]/2.0:
@@ -116,10 +119,10 @@ class game_engine():
                     self.done = True
 
         if self.right:
-            self.objects[0].right()
+            self.player.right()
 
         if self.left:
-            self.objects[0].left()
+            self.player.left()
     
     """update velocity and position of all game objects"""
     def update_physics(self):
@@ -127,16 +130,19 @@ class game_engine():
         dt = newtime-self.gametime
         self.gametime = newtime
 
+        ratioPosition = self.player.velocity[1]*dt + 0.5*self.gravity*dt**2
+        ratioVelocity = self.gravity*dt
+
         # x = x_0 + v_0*dt + 0.5*g*dt^2 
-        self.player.position[1] = self.player.position[1] + self.player.velocity[1]*dt + 0.5*self.gravity*dt**2
+        self.player.position[1] += ratioPosition
         # v = v_0 + g*dt 
-        self.player.velocity[1] = self.player.velocity[1] + self.gravity*dt
+        self.player.velocity[1] += ratioVelocity
         #make sure it doesn't go through the floor
         if self.player.position[1] > self.floor_height-70:
             self.player.position[1] = self.floor_height-70
             self.player.velocity[1] = 0.0
 
-        if self.player.position[1] < 0:
+        if 0 > self.player.position[1]:
             self.player.position[1] = 0
             self.player.velocity[1] = 0.0
             
@@ -149,12 +155,31 @@ class game_engine():
             self.event_loop()
             #advance the time for all objects
             self.update_physics()
+
+            #draw stars
+            for eachestrela in self.estrelas:
+                
+                eachestrela.descer()
+
+                if eachestrela.position[1] > self.floor_height:
+                    self.estrelas.remove(eachestrela)
+                    eachestrela = None                    
+                else:
+                    eachestrela.draw()
+
             #Draw all objects
             for gameobject in self.objects:
                 gameobject.draw()
+
+            self.player.draw()
+
             pygame.display.update()
 
+            if 100 > len(self.estrelas):
+                self.estrelas.append(estrela(self.screen,self.SCREEN_SIZE,[random.randint(1,486),0]))
+
             self.clock.tick(self.fps)
+
             
     def quit_game(self):
         pygame.display.quit()
--- game_object.py
+++ game_object.py
@@ -7,6 +7,7 @@ game object
 import pygame_sdl2
 pygame_sdl2.import_as_pygame()
 import pygame
+import random
 
 class game_object():
     #
@@ -53,6 +54,7 @@ class game_object():
             self.position[0] = 0
 
 class button():
+
     def __init__(self, screen, screenSize, color = (0,0,0), position=[0,0], size=[20,20]):
         self.position = position
         self.screen = screen
@@ -79,3 +81,112 @@ class button():
 
     def desligar(self):
         self.color = self.color_init
+
+class estrela():
+
+    def __init__(self, screen, screenSize, position=[0,0]):
+        self.position = position
+        self.screen = screen
+        self.color = (255,255,255)
+        self.screenSize = screenSize
+        self.size = (1,1)
+        self.size_info = pygame.display.Info()
+        self.velocity = random.randint(1,10)
+
+    def draw(self):
+
+        ratioX = self.screenSize[0] / 486.0
+        ratioY = self.screenSize[1] / 864.0
+
+        self.lolX = self.size[0]*ratioX
+        self.lolY = self.size[1]*ratioY
+
+        position = ((self.position[0]*ratioX)-self.lolX/2.0, (self.position[1]*ratioY)-self.lolY/2.0, self.lolX, self.lolY)
+
+        pygame.draw.rect(self.screen, self.color, position)
+
+    def descer(self):
+        
+        self.position[1] += self.velocity
+
+class Player(pygame.sprite.Group):
+
+    width = 32
+    height = 28
+
+    def __init__(self, x, y):
+
+        super(Player,self).__init__()
+
+        self.group = pygame.sprite.Group()
+        
+        base = pygame.sprite.Sprite()
+
+        base.image = pygame.Surface([self.width, 8])
+
+        base.rect = base.image.get_rect()
+
+        base.rect.x = x
+        base.rect.y = y + 24
+
+        self.base = base
+
+        self.group.add(self.base)
+
+        base = pygame.sprite.Sprite()
+
+        base.image = pygame.Surface([6, 22])
+
+        base.rect = base.image.get_rect()
+
+        base.rect.x = x + 14
+        base.rect.y = y + 2
+
+        self.tronco = base
+
+        self.group.add(self.tronco)
+
+        self.imagem = pygame.sprite.Sprite()
+
+        self.imagem.image = pygame.image.load('images/mainship_t.png')
+
+        self.imagem.rect = self.imagem.image.get_rect()
+
+        self.imagem.rect.x = x
+        self.imagem.rect.y = y
+
+        self.pos = [False,False,False,False]
+
+        self.add(self.imagem)
+
+    def right(self):
+
+        self.position[0] += 10
+
+        if self.position[0] > 454:
+            self.position[0] -= 10
+        else:
+            for sprite in self.group_grey:
+                sprite[0] += 10
+            for sprite in self.group_darkblue:
+                sprite[0] += 10
+            for sprite in self.group_darkgrey:
+                sprite[0] += 10
+            for sprite in self.group_purple:
+                sprite[0] += 10
+
+    def left(self):
+
+        self.position[0] -= 10
+
+        if 0 > self.position[0]:
+            self.position[0] += 10
+        else:
+            for sprite in self.group_grey:
+                sprite[0] -= 10
+            for sprite in self.group_darkblue:
+                sprite[0] -= 10
+            for sprite in self.group_darkgrey:
+                sprite[0] -= 10
+            for sprite in self.group_purple:
+                sprite[0] -= 10
